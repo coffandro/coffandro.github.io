abbf16e6acb4a5bf8224067f134aff3fc830eef6
Frida Rosenaa <macrosedk@gmail.com>
2026-02-06 17:37:57 +0100

    Revert player code

--- games/src/games/player.c
+++ games/src/games/player.c
@@ -22,46 +22,25 @@ void update_player() {
 
     Vector2 move_dir;
 
-    // -------------------------------
-    // 1. Compute smooth blend factor
-    // -------------------------------
-    // 0 = move directly, 1 = orbit
-    float raw_blend = (dist - ORBIT_START) / (ORBIT_END - ORBIT_START);
-    raw_blend = Clamp(raw_blend, 0.0f, 1.0f);
+    // Only orbit if the player is far away
+    if (dist < ORBIT_THRESHOLD) {
+        Vector2 radial_dir = Vector2Scale(to_mouse, 1.0f / dist);
+        Vector2 tangent_dir = (Vector2){ radial_dir.y, -radial_dir.x };
 
-    // Apply easing for smoother transition (cubic ease-in/out)
-    float blend = raw_blend * raw_blend * (3.0f - 2.0f * raw_blend);
+        float radial_error = (dist - ORBIT_RADIUS) / ORBIT_RADIUS;
+        radial_error = Clamp(radial_error, -1.0f, 1.0f);
 
-    // -------------------------------
-    // 2. Compute orbiting forces
-    // -------------------------------
-    Vector2 radial_dir = Vector2Scale(to_mouse, 1.0f / dist);
-    Vector2 tangent_dir = (Vector2){ radial_dir.y, -radial_dir.x };
+        Vector2 radial_force = Vector2Scale(radial_dir, -radial_error * RADIAL_WEIGHT);
+        Vector2 tangent_force = Vector2Scale(tangent_dir, TANGENT_WEIGHT);
 
-    float radial_error = (dist - ORBIT_RADIUS) / ORBIT_RADIUS;
-    radial_error = Clamp(radial_error, -1.0f, 1.0f);
+        Vector2 steering = Vector2Multiply(Vector2Add(tangent_force, radial_force), (Vector2){-1,-1});
+        move_dir = Vector2Normalize(steering);
+    } else {
+        // Move directly toward the target if close
+        move_dir = Vector2Normalize(to_mouse);
+    }
 
-    Vector2 radial_force = Vector2Scale(radial_dir, -radial_error * RADIAL_WEIGHT);
-    Vector2 tangent_force = Vector2Scale(tangent_dir, TANGENT_WEIGHT);
-
-    Vector2 orbit_force = Vector2Normalize(Vector2Multiply(Vector2Add(radial_force, tangent_force), (Vector2){-1, -1}));
-
-    // -------------------------------
-    // 3. Compute direct movement
-    // -------------------------------
-    Vector2 direct_force = Vector2Normalize(to_mouse);
-
-    // -------------------------------
-    // 4. Blend the two
-    // -------------------------------
-    move_dir = Vector2Normalize(Vector2Add(
-        Vector2Scale(direct_force, 1.0f - blend),
-        Vector2Scale(orbit_force, blend)
-    ));
-
-    // -------------------------------
-    // 5. Handle rotation
-    // -------------------------------
+    // Rotation
     float target_rotation = atan2f(move_dir.y, move_dir.x) * RAD2DEG + 90.0f;
     float delta_angle = target_rotation - rotation;
 
@@ -74,20 +53,17 @@ void update_player() {
 
     rotation += delta_angle;
 
-    // -------------------------------
-    // 6. Move player
-    // -------------------------------
+    // Movement
     pos = Vector2Add(pos, Vector2Scale(move_dir, SPEED * GetFrameTime()));
 
-    // -------------------------------
-    // 7. Screen wrap
-    // -------------------------------
+    // Screen wrap
     Vector2 screen = (Vector2){ GetScreenWidth(), GetScreenHeight() };
     if (pos.x < 0) pos.x = screen.x;
     else if (pos.x > screen.x) pos.x = 0;
     if (pos.y < 0) pos.y = screen.y;
     else if (pos.y > screen.y) pos.y = 0;
 }
+
 
 void draw_player() {
     DrawTextureEx(
--- games/src/games/player.h
+++ games/src/games/player.h
@@ -8,9 +8,8 @@
 #define ORBIT_RADIUS   50.0f
 #define TANGENT_WEIGHT 1.0f
 #define RADIAL_WEIGHT  1.25f
-#define ROTATION_SPEED 360.0f // degrees per second
-#define ORBIT_START 200
-#define ORBIT_END 100
+#define ROTATION_SPEED 180.0f // degrees per second
+#define ORBIT_THRESHOLD 100
 
 void init_player();
 void update_player();
